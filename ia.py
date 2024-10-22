8b502c4a812bf90bc6d09ff961dd8d2485672d2d
Kauan Henrique <kauanlrez@gmail.com>
2024-10-22 09:51:12 -0300

    Add files via upload

---
+++ ia.py
@@ -0,0 +1,33 @@
+import os
+from groq import Groq
+
+
+os.environ["GROQ_API_KEY"] = "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"
+
+client = Groq (
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+
+messages=[]
+
+while True:
+    usuario = input ("Digite uma mensagem ou 'sair' para encerrar: ")
+
+    if usuario.lower() == 'sair':
+        print("Conversa encerrada.")
+        break
+
+    
+    messages.append({"role": "user", "content": usuario})
+
+chat_completion = client.chat.completions.create(
+    messages=messages,
+    model="llama-3.1-70b-versatile",
+)
+
+resposta = chat_completion.choices[0].message.content
+print("Resposta: ", resposta)
+
+
+messages.append({"role": "assistant", "content": resposta})
